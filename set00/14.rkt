1d20b25e16c2130ae41c4bd98b52819670cafa04
Shawn Valliere <valliesw@ccs.neu.edu>
2014-01-19 23:04:50 -0500

    Exercise 14 and lab notebook update

---
+++ set00/14.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |14|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+; When (define-struct student (id name major)) is executed, Racket will define
+; the following:
+;  - make-student
+;  - student?
+;  - student-id
+;  - student-name
+;  - student-major
